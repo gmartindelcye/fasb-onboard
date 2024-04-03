8fd392f4ac2a8405daaeaf417f7b6611da6db708
Gabriel Martin del Campo y Eternod <gmartindelcye@gmail.com>
2024-04-03 15:11:49 -0600

    flake8 users files

--- populate/first_user.py
+++ populate/first_user.py
@@ -4,18 +4,23 @@ from database import engine
 from settings import settings
 from security import get_password_hash
 
+
 def create_first_user():
     with Session(engine) as session:
-        user = session.exec(select(User).where(User.username == settings['FIRST_SUPERUSER'])).first()
+        statement = select(User).where(
+                      User.username == settings['FIRST_SUPERUSER']
+                    )
+        user = session.exec(statement).first()
         if not user:
             user = User(
-                        username=settings['FIRST_SUPERUSER'], 
-                        password=get_password_hash(settings['FIRST_SUPERUSER_PASSWORD']), 
-                        is_superuser=True
-                    )
+                     username=settings['FIRST_SUPERUSER'],
+                     password=get_password_hash(
+                                settings['FIRST_SUPERUSER_PASSWORD']
+                              ),
+                     is_superuser=True
+                   )
             session.add(user)
             session.commit()
             session.refresh(user)
             return user
         return {"message": "First user already exists"}
-
--- routes/user.py
+++ routes/user.py
@@ -1,13 +1,9 @@
-from datetime import datetime, timedelta, timezone
-from typing import Annotated, Optional, List
-from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import Depends, HTTPException, APIRouter
 from sqlmodel import Session, select
-from pydantic import BaseModel
-from passlib.context import CryptContext
-from settings import settings
 from database import get_session
-from models import User, UserBase, UserRead, UserCreate, UserPassword, UserActive, UserSuperuser
+from models import (User, UserBase, UserRead, UserCreate, UserPassword,
+                    UserActive, UserSuperuser)
 from security import oauth2_scheme, get_password_hash
 
 router = APIRouter(
@@ -19,20 +15,21 @@ router = APIRouter(
 
 @router.get("/", response_model=list[UserRead])
 async def get_users(
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+            token: Annotated[str, Depends(oauth2_scheme)],
+            session: Session = Depends(get_session)
+          ):
     users = session.exec(select(User)).all()
     return users
 
 
 @router.post("/", response_model=UserRead)
 async def create_User(
-        user: UserCreate, 
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
-    user_exist = session.exec(select(User).where(User.username == user.username)).first()
+            user: UserCreate,
+            token: Annotated[str, Depends(oauth2_scheme)],
+            session: Session = Depends(get_session)
+          ):
+    statement = select(User).where(User.username == user.username)
+    user_exist = session.exec(statement).first()
     if user_exist:
         raise HTTPException(status_code=400, detail="User already exists")
     user = User(
@@ -49,10 +46,10 @@ async def create_User(
 
 @router.get("/{user_id}", response_model=User)
 async def get_User(
-        user_id: int, 
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+            user_id: int,
+            token: Annotated[str, Depends(oauth2_scheme)],
+            session: Session = Depends(get_session)
+          ):
     user = session.get(User, user_id)
     if not User:
         raise HTTPException(status_code=404, detail="User not found")
@@ -61,11 +58,11 @@ async def get_User(
 
 @router.patch("/{user_id}", response_model=UserRead)
 async def update_User(
-        user_id: int, 
-        user: UserBase,
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+            user_id: int,
+            user: UserBase,
+            token: Annotated[str, Depends(oauth2_scheme)],
+            session: Session = Depends(get_session)
+          ):
     db_user = session.get(User, user_id)
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
@@ -78,13 +75,12 @@ async def update_User(
     return db_user
 
 
-
 @router.delete("/{user_id}")
 async def delete_User(
-        user_id: int, 
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+            user_id: int,
+            token: Annotated[str, Depends(oauth2_scheme)],
+            session: Session = Depends(get_session)
+          ):
     user = session.get(User, user_id)
     if not User:
         raise HTTPException(status_code=404, detail="User not found")
@@ -95,11 +91,11 @@ async def delete_User(
 
 @router.post("/password/{user_id}")
 def change_password(
-        user_id: int,
-        password: UserPassword,
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+      user_id: int,
+      password: UserPassword,
+      token: Annotated[str, Depends(oauth2_scheme)],
+      session: Session = Depends(get_session)
+     ):
     user = session.get(User, user_id)
     if not User:
         raise HTTPException(status_code=404, detail="User not found")
@@ -112,11 +108,11 @@ def change_password(
 
 @router.post("/active/{user_id}")
 def change_active(
-        user_id: int,
-        active: UserActive,
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+      user_id: int,
+      active: UserActive,
+      token: Annotated[str, Depends(oauth2_scheme)],
+      session: Session = Depends(get_session)
+     ):
     user = session.get(User, user_id)
     if not User:
         raise HTTPException(status_code=404, detail="User not found")
@@ -129,11 +125,11 @@ def change_active(
 
 @router.post("/superuser/{user_id}")
 def change_superuser(
-        user_id: int,
-        superuser: UserSuperuser,
-        token: Annotated[str, Depends(oauth2_scheme)],
-        session: Session = Depends(get_session)
-    ):
+      user_id: int,
+      superuser: UserSuperuser,
+      token: Annotated[str, Depends(oauth2_scheme)],
+      session: Session = Depends(get_session)
+     ):
     user = session.get(User, user_id)
     if not User:
         raise HTTPException(status_code=404, detail="User not found")
